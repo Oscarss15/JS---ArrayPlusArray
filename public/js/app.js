240042cb4096f36998e3288f3c90da6fbc58d8dc
Oscar <oscar15-91@hotmail.com>
2024-06-26 21:58:12 +0200

    Añadido jss del ejercicio

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+const arraynumbers1 = [1, 2, 3];
+const arraynumbers2 = [5, 7, 9];
+let resultadoSuma = 0;
+let resultadoSuma2 = 0;
+
+function sumaArrays() {
+  for (let i = 0; i < arraynumbers1.length; i++) {
+    let numero = arraynumbers1[i];
+    resultadoSuma = resultadoSuma + numero;
+  }
+  for (let i = 0; i < arraynumbers2.length; i++) {
+    let numero2 = arraynumbers2[i];
+    resultadoSuma2 = resultadoSuma2 + numero2;
+  }
+
+  return resultadoSuma + resultadoSuma2;
+}
+function mostrarResultado() {
+  console.log(sumaArrays());
+}
+mostrarResultado();
